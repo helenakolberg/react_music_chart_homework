ab4a91eaa24337352970a49a112a9380007cb80a
Helena <inger.helena@gmail.com>
2020-09-15 22:28:21 +0100

    fetched data from api

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class SongContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            songs: []
+        };
+    }
+
+    componentDidMount() {
+        const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => this.setState({ songs: data.feed.entry }))
+            .catch(err => console.log(err));
+
+    }
+
     render() {
         return (
             <>
+                <SongList songs={this.state.songs} />
             </>
         );
     }
