1dbf4e6a8950984b92fff5399347e1782526e8db
Helena <inger.helena@gmail.com>
2020-09-15 22:45:20 +0100

    generated chart table

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 const SongList = (props) => {
+
+    const songChart = props.songs.map((song) => {
+        let chartPosition = props.songs.indexOf(song) + 1;
+        return (
+            <tr key={song.id.attributes['im:id']}>
+                <td>{chartPosition}</td>
+                <td>{song['im:name'].label}</td>
+                <td>{song['im:artist'].label}</td>
+            </tr>
+        )
+    });
+
     return (
         <>
+            <table>
+                <tr>
+                    <th>Chart Position</th>
+                    <th>Song</th>
+                    <th>Artist</th>
+                </tr>
+                {songChart}
+            </table>
         </>
     )
 }
